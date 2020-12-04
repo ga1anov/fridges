b53502dd7b3a614e242747a6e766def9b0306006
ga1anov <59095314+ga1anov@users.noreply.github.com>
2020-12-04 16:17:36 +0500

    действия с заявками

---
+++ reqdobav.php
@@ -0,0 +1,77 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		
+		<title>ЛР4-5 Галанов Иван</title> 
+<style>
+body {
+background: #c7b39b; /* Цвет фона и путь к файлу */
+color: #fff; /* Цвет текста */
+}
+</style> 
+	</head>
+	<body>
+		<h4>Галанов И.С.</h4>
+		<h1>Лабораторная работа №4-5</h1>
+		<h2>Вариант 6</h2>
+<H3>Добавление заявки:</H3>
+		<?php 
+		$connect = mysqli_connect("localhost", "root", "root", "galanov")or die("Невозможно подключиться к серверу");
+		mysqli_query($connect, "SET NAMES utf8");
+
+$rows1 = mysqli_query($connect, "SELECT id, brand FROM fridge ORDER BY id");
+$i=0;
+        while ($st = mysqli_fetch_array($rows1)) {
+            $brand[$i] = $st['brand'];
+            $id_zh[$i] = $st['id'];
+            $i++;
+        }
+$rows2 = mysqli_query($connect, "SELECT id, name FROM service ORDER BY id");
+$j=0;
+        while ($st1 = mysqli_fetch_array($rows2)) {
+            $name[$j] = $st1['name'];
+            $id_zh1[$j] = $st1['id'];
+            $j++;
+        }
+?>
+<form action="reqdobav1.php" method="get">
+Дата начала ремонта: <input name="daten" size="20" type="date">
+<br>Дата окончания: <input name="dateo" size="20" type="date">
+<br>Холодильник: 
+
+<select name='brand'>
+        <?php
+        for($i = 0; $brand[$i] != null; $i++): ?>
+            <option value="<?=$brand[$i].$i?>"><?=$brand[$i]?></option>
+        <?php endfor;
+        for($i = 0; $brand[$i] != null; $i++){
+$brand = "num". $i;
+$value = "" . $id_zh[$i];
+print "<input type='hidden' name='".$brand."' value='".$value."'>";
+}
+       ?>
+<br>
+Сервисный центр:
+<select name='name'>
+        <?php
+        for($j = 0; $name[$j] != null; $j++): ?>
+            <option value="<?=$name[$j].$j?>"><?=$name[$j]?></option>
+        <?php endfor;
+        for($j = 0; $name[$j] != null; $j++){
+$name = "numn". $j;
+$value1 = "" . $id_zh1[$j];
+print "<input type='hidden' name='".$name."' value='".$value1."'>";
+}
+        ?>
+        </select><br>
+<br>ФИО клиента: <input name="fio" size="20" type="varchar">
+<br>Стоимость: <input name="cost" size="20" type="varchar">
+</textarea>
+<p><input name="add" type="submit" value="Добавить">
+</form>
+<p>  
+
+<p><a href="laba4.php">назад</a></p>	
+</div>
+	</body>
+</html>
---
+++ reqdobav1.php
@@ -0,0 +1,50 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		
+		<title>ЛР4-5 Галанов Иван</title> 
+<style>
+body {
+background: #c7b39b; /* Цвет фона и путь к файлу */
+color: #fff; /* Цвет текста */
+}
+</style> 
+	</head>
+	<body>
+		<h4>Галанов И.С.</h4>
+		<h1>Лабораторная работа №4-5</h1>
+		<h2>Вариант 6</h2>
+<H3>Добавление заявки:</H3>
+
+		<?php
+$connect = mysqli_connect("localhost", "root", "root", "galanov" ) or die ("Невозможно подключиться к серверу"); // установление соединения с сервером
+//Кодировка данных получаемых из базы
+mysqli_query($connect, "SET NAMES utf8");
+	$get_fio = $_GET['brand'];
+	$brand = substr($_GET['brand'], 0, (strlen($_GET['brand'])-1));
+	$num = substr($_GET['brand'], -1, 1);
+	$v = "num". $num;
+	$hid = "" . $_GET[$v];
+$str = (int)$hid;
+
+$get_fio1 = $_GET['name'];
+	$name1 = substr($_GET['name'], 0, (strlen($_GET['name'])-1));
+	$num1 = substr($_GET['name'], -1, 1);
+	$v1 = "numn". $num1;
+	$hid1 = "" . $_GET[$v1];
+$str1 = (int)$hid1;
+
+$sql_add="INSERT INTO request SET daten='".$_GET['daten']."', dateo='".$_GET['dateo']."', id_f='" .$str ."', name_f='".$brand."', id_c='" .$str1.
+"', name_c='".$name1."', fio='".$_GET['fio']."', cost='".$_GET['cost']."'";
+
+mysqli_query($connect, $sql_add);
+if (mysqli_affected_rows($connect)>0) // если нет ошибок при выполнении запроса
+{ print "<p>Спасибо, вы зарегистрировали заявку в базе данных.";
+print "<p><a href=\"laba4.php\"> Вернуться к списку</a>"; }
+else { print "Ошибка сохранения.".mysqli_error($connect)."a href=\"laba4.php\"> Вернуться к списку</a>"; }
+
+?>
+		
+</div>
+	</body>
+</html>
